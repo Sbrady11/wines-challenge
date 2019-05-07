5594ce767a36e08b575fe2a726d45350506220db
Simon Brady <simonbrady@mac.com>
2019-05-06 22:56:05 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './app.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDom.render(<App />, document.getElementById('root'))
